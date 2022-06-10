e0dea8407effa8ddd865910c71042e9f7e043129
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 15:22:14 -0300

    Add files via upload

---
+++ AMS_Ex1_22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AMS_Ex1_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeMinima = 20;
+            int idadeMaxima = 50;
+            int mediaIdade = (idadeMinima + idadeMaxima)/2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"A média dessas idades é: {mediaIdade}");
+            Console.WriteLine("");
+        }
+    }
+}
